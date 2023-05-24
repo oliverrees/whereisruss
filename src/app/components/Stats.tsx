2b41b1c63bd2a4504e0a4a3f111fab68f1cf65a0
oliverrees <oliver@oliverrees.co.uk>
2023-05-24 14:10:04 +0100

    Render fix

--- src/app/components/Stats.tsx
+++ src/app/components/Stats.tsx
@@ -1,9 +1,7 @@
-"use client";
-import React, { useState } from "react";
 import { differenceInDays, addDays, format } from "date-fns";
-import { Switch } from "@headlessui/react";
 import Link from "next/link";
 import GetWeather from "./GetWeather";
+import StatsTable from "./StatsTable";
 
 type Props = {
   totalDistance: number;
@@ -11,40 +9,7 @@ type Props = {
   coords: any;
 };
 
-function classNames(...classes: any[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
 const Stats = ({ totalDistance, lastDistance, coords }: Props) => {
-  const [miles, setMiles] = useState(false);
-  const daysRemaining = ((15000 - totalDistance) / 60).toFixed(0);
-  const endDate = addDays(new Date(), parseInt(daysRemaining));
-  const stats = [
-    {
-      label: "Yesterday's Distance",
-      value: {
-        km: lastDistance.toFixed(0),
-        miles: (lastDistance * 0.621371).toFixed(0),
-      },
-    },
-    {
-      label: "Total distance",
-      value: {
-        km: totalDistance.toFixed(0),
-        miles: (totalDistance * 0.621371).toFixed(0),
-      },
-    },
-    {
-      label: "Est. Distance remaining",
-      value: {
-        km: (15000 - parseInt(totalDistance.toFixed(0))).toFixed(0),
-        miles: (
-          (15000 - parseInt(totalDistance.toFixed(0))) *
-          0.621371
-        ).toFixed(0),
-      },
-    },
-  ];
   const timeSinceStart = differenceInDays(new Date(), new Date(2023, 3, 21));
   return (
     <div className="fixed bottom-0 md:bottom-10 left-0 md:left-10 right-0 md:right-10 overflow-hidden pointer-events-none shadow-lg md:rounded-lg z-20 md:max-w-sm">
@@ -84,83 +49,8 @@ const Stats = ({ totalDistance, lastDistance, coords }: Props) => {
           </Link>
         </div>
       </div>
-      <div className="bg-white py-0.5 md:py-0">
-        <table className="w-full divide-y divide-gray-300 ">
-          <tbody className="divide-y divide-gray-200">
-            {stats.map((stat) => (
-              <tr key={stat.label}>
-                <td className="px-4 py-0 md:py-4 text-xs md:text-sm font-medium text-gray-900">
-                  {stat.label}
-                </td>
-                <td className="px-4 py-2 md:py-4 text-xs md:text-sm text-gray-500">
-                  {miles ? stat.value.miles : stat.value.km}
-                  &nbsp;
-                  {miles ? "miles" : "km"}
-                </td>
-              </tr>
-            ))}
-            <tr>
-              <td className="px-4 py-0 md:py-4 text-xs md:text-sm font-medium text-gray-900">
-                Est. Days Remaining
-              </td>
-              <td className="px-4 py-2 md:py-4 text-xs md:text-sm text-gray-500">
-                {daysRemaining}
-              </td>
-            </tr>
-            <tr>
-              <td className="px-4 py-0 md:py-4 text-xs md:text-sm font-medium text-gray-900">
-                Est. Finish Date
-              </td>
-              <td className="px-4 py-2 md:py-4 text-xs md:text-sm text-gray-500">
-                {format(endDate, "dd/MM/yyyy")}
-              </td>
-            </tr>
-            {/* <tr>
-            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-              Weather at last location
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              <GetWeather lat={coords[0][0][0]} lng={coords[0][0][1]} />
-            </td>
-          </tr> */}
-          </tbody>
-        </table>
-      </div>
-      <div
-        style={{ pointerEvents: "all" }}
-        className="z-10 relative py-2 md:py-4 flex items-center bg-gray-50 justify-center gap-x-4 text-xs md:text-sm font-semibold"
-      >
-        Kilometres
-        <UnitSwitch miles={miles} setMiles={setMiles} />
-        Miles
-      </div>
+      <StatsTable lastDistance={lastDistance} totalDistance={totalDistance} />
     </div>
-  );
-};
-
-interface UnitSwitchProps {
-  miles: boolean;
-  setMiles: any;
-}
-
-const UnitSwitch = ({ miles, setMiles }: UnitSwitchProps) => {
-  return (
-    <Switch
-      checked={miles}
-      onChange={setMiles}
-      className={classNames(
-        "bg-gray-200 relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none"
-      )}
-    >
-      <span className="sr-only">Use setting</span>
-      <span
-        aria-hidden="true"
-        className={classNames(
-          miles ? "translate-x-5" : "translate-x-0",
-          "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
-        )}
-      />
-    </Switch>
   );
 };
 
---
+++ src/app/components/StatsTable.tsx
@@ -0,0 +1,125 @@
+"use client";
+import { Switch } from "@headlessui/react";
+import { useState } from "react";
+import { format, addDays } from "date-fns";
+interface Props {
+  totalDistance: number;
+  lastDistance: number;
+}
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const StatsTable = ({ lastDistance, totalDistance }: Props) => {
+  const daysRemaining = ((15000 - totalDistance) / 60).toFixed(0);
+  const endDate = addDays(new Date(), parseInt(daysRemaining));
+  const [miles, setMiles] = useState(false);
+  const stats = [
+    {
+      label: "Yesterday's Distance",
+      value: {
+        km: lastDistance.toFixed(0),
+        miles: (lastDistance * 0.621371).toFixed(0),
+      },
+    },
+    {
+      label: "Total distance",
+      value: {
+        km: totalDistance.toFixed(0),
+        miles: (totalDistance * 0.621371).toFixed(0),
+      },
+    },
+    {
+      label: "Est. Distance remaining",
+      value: {
+        km: (15000 - parseInt(totalDistance.toFixed(0))).toFixed(0),
+        miles: (
+          (15000 - parseInt(totalDistance.toFixed(0))) *
+          0.621371
+        ).toFixed(0),
+      },
+    },
+  ];
+
+  return (
+    <div className="bg-white py-0.5 md:py-0">
+      <table className="w-full divide-y divide-gray-300 ">
+        <tbody className="divide-y divide-gray-200">
+          {stats.map((stat) => (
+            <tr key={stat.label}>
+              <td className="px-4 py-0 md:py-4 text-xs md:text-sm font-medium text-gray-900">
+                {stat.label}
+              </td>
+              <td className="px-4 py-2 md:py-4 text-xs md:text-sm text-gray-500">
+                {miles ? stat.value.miles : stat.value.km}
+                &nbsp;
+                {miles ? "miles" : "km"}
+              </td>
+            </tr>
+          ))}
+          <tr>
+            <td className="px-4 py-0 md:py-4 text-xs md:text-sm font-medium text-gray-900">
+              Est. Days Remaining
+            </td>
+            <td className="px-4 py-2 md:py-4 text-xs md:text-sm text-gray-500">
+              {daysRemaining}
+            </td>
+          </tr>
+          <tr>
+            <td className="px-4 py-0 md:py-4 text-xs md:text-sm font-medium text-gray-900">
+              Est. Finish Date
+            </td>
+            <td className="px-4 py-2 md:py-4 text-xs md:text-sm text-gray-500">
+              {format(endDate, "dd/MM/yyyy")}
+            </td>
+          </tr>
+          {/* <tr>
+            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+              Weather at last location
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+              <GetWeather lat={coords[0][0][0]} lng={coords[0][0][1]} />
+            </td>
+          </tr> */}
+        </tbody>
+      </table>
+      <div
+        style={{ pointerEvents: "all" }}
+        className="z-10 relative py-2 md:py-4 flex items-center bg-gray-50 justify-center gap-x-4 text-xs md:text-sm font-semibold"
+      >
+        Kilometres
+        <UnitSwitch miles={miles} setMiles={setMiles} />
+        Miles
+      </div>
+    </div>
+  );
+};
+
+interface UnitSwitchProps {
+  miles: boolean;
+  setMiles: any;
+}
+
+const UnitSwitch = ({ miles, setMiles }: UnitSwitchProps) => {
+  return (
+    <Switch
+      checked={miles}
+      onChange={setMiles}
+      className={classNames(
+        "bg-gray-200 relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none"
+      )}
+    >
+      <span className="sr-only">Use setting</span>
+      <span
+        aria-hidden="true"
+        className={classNames(
+          miles ? "translate-x-5" : "translate-x-0",
+          "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+        )}
+      />
+    </Switch>
+  );
+};
+
+export default StatsTable;
