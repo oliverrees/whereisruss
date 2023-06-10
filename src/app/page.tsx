23940010ce78412b499c02e374572f8d1ef51c86
oliverrees <5574826+oliverrees@users.noreply.github.com>
2023-06-10 12:19:30 +0100

    Revalidate change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Loading from "./components/Loading";
 import { processData } from "./functions/processData";
 import LiveWeather from "./components/LiveWeather";
 
-export const revalidate = 0;
+export const revalidate = 600;
 
 async function getLiveWeather(data: any) {
   if (!data) return;
